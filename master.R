4acda541fd7edc4800f7ed3680f23e106627f590
meganjane333 <84370721+meganjane333@users.noreply.github.com>
2024-04-11 13:51:25 +0100

    update - cleaned code
    Using master script and source(script_name) to help clean up code in qmd file

---
+++ master.R
@@ -0,0 +1,19 @@
+# Master script
+
+## load libaries
+source("packages.R")
+
+## load data
+source("data_load.R")
+
+## load ggplot themes 
+source("themes.R")
+
+## load colour pallettes
+source("pallettes.R")
+
+## load data prep
+source("data_prep.R")
+
+## load leaflet map prep
+source("leaflet_map.R")
